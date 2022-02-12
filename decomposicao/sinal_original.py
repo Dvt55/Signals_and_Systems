205eb444e5590a391572c1733863341af8c13f08
David <dmelo8185@gmail.com>
2022-02-11 21:16:26 -0300

    inclusão de arquivos da decomposicao de sinais

---
+++ decomposicao/sinal_original.py
@@ -0,0 +1,24 @@
+from math  import *
+from numpy import *
+from pylab import *
+
+# sinal x[n] = 3*n + 5
+def x(n):
+    return n**3 - 5
+ 
+fig = figure(1)
+fig.set_size_inches((8., 8.))
+
+# Formulas de decomposição de par e ímpar
+n = arange(-4., 5.)
+par = 0.5 * (x(n) + x(-n))
+impar = 0.5 * (x(n) - x(-n))
+
+# Plot do gráfico
+var =  fig.add_subplot(2, 1, 1)
+var.stem(n, x(n), "k-", "ko", "k-")
+var.set_xlim([ -4., 4. ])
+var.set_ylim([ -40, 40 ])
+var.set_title("Sinal original $n^3-5$")
+var.set_xticks(n)
+savefig("sinal-original.png", bbox_inches='tight')
